c17575264367c52e8304ef4f877d2134bb25c17a
Docs Robot <robot@example.com>
2025-02-18 21:18:53 +0000

    Committing documentation changes

--- docs/class_basic_stepper.js
+++ docs/class_basic_stepper.js
@@ -1,5 +1,6 @@
 var class_basic_stepper =
 [
+    [ "BasicStepper", "class_basic_stepper.html#aee51fb96411df301565b8b644563a974", null ],
     [ "BasicStepper", "class_basic_stepper.html#af33e385bf864ededd1d1c799f36b85d5", null ],
     [ "BasicStepper", "class_basic_stepper.html#a0fb7170151aaa1bb87908393a7143bf8", null ]
 ];
--- docs/search/all_2.js
+++ docs/search/all_2.js
@@ -3,6 +3,6 @@ var searchData=
   ['basic_20stepper_20driver_20arduino_20library_0',['Basic Stepper Driver Arduino library',['../index.html',1,'']]],
   ['basic_2dstepper_2ddriver_2dsoldered_2ecpp_1',['Basic-Stepper-Driver-SOLDERED.cpp',['../_basic-_stepper-_driver-_s_o_l_d_e_r_e_d_8cpp.html',1,'']]],
   ['basic_2dstepper_2ddriver_2dsoldered_2eh_2',['Basic-Stepper-Driver-SOLDERED.h',['../_basic-_stepper-_driver-_s_o_l_d_e_r_e_d_8h.html',1,'']]],
-  ['basicstepper_3',['BasicStepper',['../class_basic_stepper.html',1,'BasicStepper'],['../class_basic_stepper.html#af33e385bf864ededd1d1c799f36b85d5',1,'BasicStepper::BasicStepper(uint8_t interface, uint8_t pin1, uint8_t pin2, uint8_t pin3=0, uint8_t pin4=0, bool enable=true)'],['../class_basic_stepper.html#a0fb7170151aaa1bb87908393a7143bf8',1,'BasicStepper::BasicStepper(void(*forward)(), void(*backward)())']]],
+  ['basicstepper_3',['BasicStepper',['../class_basic_stepper.html',1,'BasicStepper'],['../class_basic_stepper.html#aee51fb96411df301565b8b644563a974',1,'BasicStepper::BasicStepper()'],['../class_basic_stepper.html#af33e385bf864ededd1d1c799f36b85d5',1,'BasicStepper::BasicStepper(uint8_t interface, uint8_t pin1, uint8_t pin2, uint8_t pin3=0, uint8_t pin4=0, bool enable=true)'],['../class_basic_stepper.html#a0fb7170151aaa1bb87908393a7143bf8',1,'BasicStepper::BasicStepper(void(*forward)(), void(*backward)())']]],
   ['board_20compatibility_4',['Board compatibility',['../index.html#autotoc_md4',1,'']]]
 ];
