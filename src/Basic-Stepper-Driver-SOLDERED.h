0522279d752c1226450703657aa0425a107fa08d
Robert Sorić <robert.soric@gmail.com>
2025-02-18 22:13:38 +0100

    Fixed for compile tests

--- src/Basic-Stepper-Driver-SOLDERED.h
+++ src/Basic-Stepper-Driver-SOLDERED.h
@@ -8,18 +8,24 @@
  * @authors     @ soldered.com
  ***************************************************/
 
- #ifndef __STEPPER_DRIVER__
- #define __STEPPER_DRIVER__
- 
- #include "Arduino.h"
- #include "libs/AccelStepper/src/AccelStepper.h"
- 
- class BasicStepper : public AccelStepper
- {
- public:
-     BasicStepper(uint8_t interface, uint8_t pin1, uint8_t pin2, uint8_t pin3 = 0, uint8_t pin4 = 0, bool enable = true)
-         : AccelStepper(interface, pin1, pin2, pin3, pin4, enable) {}
- };
- 
- #endif
- 
+#ifndef __STEPPER_DRIVER__
+#define __STEPPER_DRIVER__
+
+#include "Arduino.h"
+#include "libs/AccelStepper/src/AccelStepper.h"
+
+class BasicStepper : public AccelStepper
+{
+  public:
+    BasicStepper(uint8_t interface, uint8_t pin1, uint8_t pin2, uint8_t pin3 = 0, uint8_t pin4 = 0, bool enable = true)
+        : AccelStepper(interface, pin1, pin2, pin3, pin4, enable)
+    {
+    }
+
+    // Alternate constructor for function pointer-based stepping (forward/backward function callbacks)
+    BasicStepper(void (*forward)(), void (*backward)()) : AccelStepper(forward, backward)
+    {
+    }
+};
+
+#endif
